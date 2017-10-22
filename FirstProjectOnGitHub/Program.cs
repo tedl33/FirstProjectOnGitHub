9a596ad3fd45f139623cd09a237613397858178b
tadej.nadvesnik@gmail.com <tadej.nadvesnik@gmail.com>
2017-10-22 22:09:34 +0200

    FirstProjectOnGitHub

---
+++ FirstProjectOnGitHub/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstProjectOnGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FirstProjectOnGitHub");
+            Console.ReadKey();
+
+        }
+    }
+}
